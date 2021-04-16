0626b488f1a9ab9a7387152df4e18cc444d07ff8
Jay Onyidu <jay.onyidu@outlook.com>
2021-04-16 16:06:12 +0100

    updated cloudinary keys

--- client/pages/api/image-upload.js
+++ client/pages/api/image-upload.js
@@ -17,8 +17,8 @@ const cloudinary = require('cloudinary');
 //const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
